b772da322f4c71a0e8ca7146c10f909d7b3425dd
Ana <anama118118@gmail.com>
2017-02-08 13:39:13 -0500

    Refactor selectedImageView into imageSelectedWithPagingCollectionView and Add logic for imageSelectedWithPagingCollectionView and imagePickerCollectionView works synchronously

---
+++ jashBoard/jashBoard/ImageSelectedInUploadCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  ImageSelectedInUploadCollectionViewCell.swift
+//  jashBoard
+//
+//  Created by Ana Ma on 2/8/17.
+//  Copyright © 2017 Harichandan Singh. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectedInUploadCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "imageSelectedInUploadCellIdentifier"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViewHierarchy()
+        configureConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViewHierarchy() {
+        self.addSubview(imageView)
+    }
+    
+    func configureConstraints() {
+        imageView.snp.makeConstraints { (view) in
+            view.leading.top.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "default-placeholder")
+        return imageView
+    }()
+}
